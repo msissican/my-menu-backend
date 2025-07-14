22b6abe97dadd12d88ae089430ed4c60f186cb96
Xue <shengx5017@163.com>
2025-07-14 20:36:51 +0800

    /opti pydantic setting model config

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,10 @@ class Settings(BaseSettings):
             file_secret_settings,
         )
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+    }
 
 
 @lru_cache
